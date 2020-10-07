7e2cf9ca40f7d9ce7accbd0bb8a308fc8b073b85
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-10-07 21:26:10 +0500

    all data get and make use get allso and onsnapshot

--- firebase-cloudnary/pages/getdata/index.js
+++ firebase-cloudnary/pages/getdata/index.js
@@ -1,8 +1,61 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Loader from "react-loader-spinner";
+import fire from "../../config/fire-config";
 
 const data = () => {
+  const [datapic, setDatapic] = useState(null);
+  useEffect(() => {
+    fire
+      .firestore()
+      .collection("datapic")
+      .onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setDatapic(data);
+        console.log("All data", data);
+      });
+    //   .get()
+    //   .then((result) => {
+    //     const data = result.docs.map((doc) => ({
+    //       id: doc.id,
+    //       ...doc.data(),
+    //     }));
+    //     setDatapic(data);
+    //     console.log("All Data", data);
+    //   })
+    //   .catch((err) => console.log(err));
+  }, []);
+  if (!datapic) {
+    return (
+      <Loader
+        type="TailSpin"
+        color="black"
+        height={100}
+        width={100}
+        timeout={9000} //3 secs
+      />
+    );
+  }
   return (
     <>
+      <h2>Card</h2>
+      {datapic.map((item) => {
+        return (
+          <div className="card">
+            <img src={item.pic} style={{ width: "100%" }} />
+            <div className="container">
+              <h4>
+                <b>{item.body}</b>
+              </h4>
+              <p>{item.title}</p>
+            </div>
+          </div>
+        );
+      })}
+
       <Link href="/">
         <a
           style={{
