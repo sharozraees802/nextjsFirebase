0626489b03bef15c2e187e6d12ca0b98f67390ac
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-10-07 18:38:28 +0500

    datapost firebase and cloudnary

---
+++ firebase-cloudnary/components/CreatePost.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import fire from "../config/fire-config";
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (url) {
+      fire.firestore().collection("datapic").add({
+        title,
+        body,
+        pic: url,
+      });
+      setTitle("");
+      setBody("");
+      setUrl("");
+    }
+  }, [url]);
+  const postDetails = () => {
+    // console.log(title, body, image);
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "livani");
+    data.append("cloud_name", "livanifyp");
+    fetch("https://api.cloudinary.com/v1_1/livanifyp/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.url);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div
+      style={{
+        margin: "30px auto",
+        maxWidth: "500px",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <div>
+        Title
+        <br />
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        Content
+        <br />
+        <textarea
+          placeholder="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+      <div>
+        Image
+        <br />
+        <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+      </div>
+      <button type="submit" onClick={() => postDetails()}>
+        Save
+      </button>
+    </div>
+  );
+};
+export default CreatePost;
---
+++ firebase-cloudnary/config/fire-config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8-_jZ8gG4Y6gZEUhRv0JV-tMciKovILw",
+  authDomain: "ecommerce-website-3bea7.firebaseapp.com",
+  databaseURL: "https://ecommerce-website-3bea7.firebaseio.com",
+  projectId: "ecommerce-website-3bea7",
+  storageBucket: "ecommerce-website-3bea7.appspot.com",
+  messagingSenderId: "262495901620",
+  appId: "1:262495901620:web:3d80ac565637a52e1cc4e1",
+};
+
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err.stack);
+  }
+}
+
+const fire = firebase;
+export default fire;
