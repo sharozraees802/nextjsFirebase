52ad286f74d9c0088e366f3d7226b9f8f4491cbb
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-12-29 21:16:21 +0500

    pwa reactjs install reac-router-dom

---
+++ pwa_app/src/components/Navbar/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "../Screens/Home/home";
+import User from "../Screens/User/user";
+import About from "../Screens/About/about";
+import "../../App.css";
+const navbar = () => {
+  return (
+    <div>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand>
+            <Link to="/">Navbar</Link>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/user">Users</Link>
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={User} path="/user" />
+          <Route component={About} Path="/about" />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default navbar;
---
+++ pwa_app/src/components/Screens/User/user.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const user = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>User-Page</h1>
+    </div>
+  );
+};
+
+export default user;
