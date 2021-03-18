3e202d6c30a185dc920dcc7401493c7f28ad1c33
sharozraees802 <sharozraees802@gmail.com>
2021-03-18 19:32:22 +0500

    datafirebase folder working

--- datafirebase/fire-config.js
+++ datafirebase/fire-config.js
@@ -1,4 +1,24 @@
 const firebase = require("firebase");
+// import "firebase/auth";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA8-_jZ8gG4Y6gZEUhRv0JV-tMciKovILw",
+//   authDomain: "ecommerce-website-3bea7.firebaseapp.com",
+//   databaseURL: "https://ecommerce-website-3bea7.firebaseio.com",
+//   projectId: "ecommerce-website-3bea7",
+//   storageBucket: "ecommerce-website-3bea7.appspot.com",
+//   messagingSenderId: "262495901620",
+//   appId: "1:262495901620:web:3d80ac565637a52e1cc4e1",
+// };
+
+// try {
+//   firebase.initializeApp(firebaseConfig);
+// } catch (error) {
+//   if (!/already exists/.test(error.message)) {
+//     console.error("Firebase initialization error", error.stack);
+//   }
+// }
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8-_jZ8gG4Y6gZEUhRv0JV-tMciKovILw",
@@ -12,8 +32,11 @@ const firebaseConfig = {
 
 try {
   firebase.initializeApp(firebaseConfig);
-} catch (error) {
-  if (!/already exists/.test(error.message)) {
-    console.error("Firebase initialization error", error.stack);
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err);
   }
 }
+
+// const auth = firebase.auth();
+// export { auth, firebase };
--- datafirebase/index.js
+++ datafirebase/index.js
@@ -1,6 +1,6 @@
 require("./fire-config");
 const firebase = require("firebase");
-const data = require("./Products");
+// const data = require("./Products");
 
 // firebase
 //   .firestore()
@@ -11,17 +11,125 @@ const data = require("./Products");
 //   .then((res) => console.log("data send"))
 //   .catch((err) => console.log("error", err));
 
-for (let i = 0; i < data.length; i++) {
-  const element = data[i];
-  //console.log(element);
-  //console.log(i);
-  firebase
-    .firestore()
-    .collection("products")
-    .add({
-      element,
-    })
-    .then(() => console.log("data send", i))
-    .catch((err) => console.log("error", err));
-}
+// for (let i = 0; i < data.length; i++) {
+//   const element = data[i];
+//console.log(element);
+//console.log(i);
+//   firebase
+//     .firestore()
+//     .collection("products")
+//     .add({
+//       element,
+//     })
+//     .then(() => console.log("data send", i))
+//     .catch((err) => console.log("error", err));
+// }
 // Aachp9pV57urHn6M9vdu
+
+// var userdataall = [];
+// firebase
+//   .firestore()
+//   .collection("userdata")
+//   .get()
+//   .then((res) => {
+//     res.forEach((doc) => {
+//       var orderObj = doc.data();
+//       userdataall.push(orderObj.email);
+//       if ("sharozkhan802@livani.com" === orderObj.email) {
+//         break;
+//         console.log("true");
+//       } else {
+//         console.log("false");
+//       }
+//       // console.log(userdataall);
+//     });
+//   })
+//   .catch((e) => console.log(e));
+
+// //console.log(userdataall);
+
+// firebase
+//   .firestore()
+//   .collection("userdata")
+//   .where("sharozkhan802@livani.com", "==", true)
+//   .get()
+//   .then((res) => {
+//     res.forEach((doc) => {
+//       var orderObj = doc.data();
+//       console.log(orderObj);
+//     });
+//   })
+//   .catch((e) => console.log("Error:", e));
+
+// let db = firebase.firestore();
+
+// db.collection("userdata")
+//   .where("sharozkhan802@livani.com", "==", false)
+//   .get()
+//   .then(function (querySnapshot) {
+//     querySnapshot.forEach(function (doc) {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log("Error getting documents: ", error);
+//   });
+// console.log("helloworld");
+let data = [];
+var email = "sharozkhan802@livani.com";
+firebase
+  .firestore()
+  .collection("userdata")
+  .get()
+  .then((res) => {
+    res.forEach((doc) => {
+      doc.data();
+      // if (orderObj.email == "sharozkhan802@livani.com") {
+      //   console.log("this email not use");
+      // } else {
+      //   console.log("this email  use");
+      // }
+      let orderObj = doc.data();
+      //console.log(orderObj);
+      data.push(orderObj);
+      // if (orderObj.email == email) {
+      //   console.log("this user is use the email");
+      // } else {
+      //   console.log("this user is not use the email");
+      // }
+    });
+    const found = data.filter((e) => e.email == "sharozkhan@livani.com");
+    if (found.length === 0) {
+      console.log("use");
+      console.log(found);
+    } else {
+      console.log("not");
+    }
+  })
+  .catch((e) => console.log("Error:", e));
+
+// // let db = firebase
+// //   .firestore()
+// //   .collection("userdata")
+// //   .where("email", "==", "sharozkhan802@livani.com");
+// // console.log(db.get());
+
+//let array = [{ name: "raees" }, { Name: "hassan" }, { Name: "sharoz" }];
+// for (let i = 0; i < array.length; i++) {
+//   if ("hassan" == array[i]) {
+//     return console.log("found");
+//     break;
+//   } else {
+//     return console.log("nor found");
+//   }
+// }
+
+// const found = array.filter((e) => {
+//   if (e.Name == "sharoz") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(found);
