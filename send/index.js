487057eee2a57d9984409ef6fba561dde5f7e0c6
sharozraees802 <sharozraees802@gmail.com>
2021-03-29 20:56:16 +0500

    add two string

---
+++ send/index.js
@@ -0,0 +1,13 @@
+// console.log("helloworld")
+
+let variable = ""
+
+let vname = ["I received plastic trasparent cover instead of carban leather cover for mi mix 2 from AHK mobile islamabad thankyou"," zxcczcx","good product","fine usefull"]
+
+// for (let i = 0; i < vname.length ; i++) {
+    
+//     variable += vname[i] +" ";
+// }
+// console.log(variable)
+
+console.log(vname[0])
